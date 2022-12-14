077678d6e96ed21dfc3bee1668ebc82053f04d0e
agu5a <2825771+agu5a@users.noreply.github.com>
2022-12-14 05:12:17 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+﻿# AndroKat
+
+Az AndroKat 2012.10.09-én indult el, mint magyar katolikus alkalmazás android operációs rendszerrel működő telefonra.
+
+MOTTÓ
+*"Az istentisztelet, az erkölcsi élet, a hivatalok és funkciók s minden más arra való az Egyházban, hogy ezt az isteni szeretetet viszonozza Istennek, illetve megjelenítse, közvetítse a világ számára." (Barsi Balázs)*
+
+#### AndroKat alkalmazás a Google Play-en
+[play.google.com](https://play.google.com/store/apps/details?id=hu.AndroKat)
+
+#### AndroKat a Facebook-on
+[facebook](https://www.facebook.com/androkat)
+
+#### AndroKat a Twitter-en
+[![](https://img.shields.io/twitter/follow/AndroKat?style=social)](https://twitter.com/AndroKat)
+
+#### AndroKat a Youtube-on
+[![](https://img.shields.io/youtube/channel/views/UCF3mEbdkhZwjQE8reJHm4sg?style=social)](https://www.youtube.com/@androkat3634)
+
+#### AndroKat az Instagram-on
+[instagram](https://www.instagram.com/androkat_app)
---
+++ src/androkat.web/androkat.core/Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Text.Json.Serialization;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((ctx, lc) =>
+    {
+        lc
+        .ReadFrom.Configuration(ctx.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console();
+    });
+
+    builder.Services.AddControllers()
+        .AddRazorRuntimeCompilation().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+
+    builder.Services.AddRazorPages();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseStatusCodePagesWithReExecute("/Error/{0}");
+    }
+
+    var forwardedHeadersOptions = new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+    };
+    app.UseForwardedHeaders(forwardedHeadersOptions);
+
+    app.UseHttpsRedirection();
+
+    var provider = new FileExtensionContentTypeProvider();
+    provider.Mappings[".epub"] = "application/epub+zip";
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        ContentTypeProvider = provider
+    });
+
+    app.UseRouting();
+    app.UseSession(); //Call UseSession after UseRouting and before MapRazorPages
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}");
+    app.MapRazorPages();
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Stopped program because of exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
