bcf7fb0f81956a022a37b631f5b88fd4b6ead75c
arnold gulyas <arnoldgulyas@gmail.com>
2023-06-25 09:23:10 +0200

    Favorites export/import started

--- src/androkat.maui/androkat.maui/Pages/FavoriteListPage.xaml.cs
+++ src/androkat.maui/androkat.maui/Pages/FavoriteListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using androkat.maui.library.Abstraction;
+﻿using Android.OS;
+using androkat.maui.library.Abstraction;
+using androkat.maui.library.Models.Entities;
 using androkat.maui.library.ViewModels;
 using CommunityToolkit.Maui.Alerts;
 using CommunityToolkit.Maui.Core;
@@ -55,6 +57,82 @@ public partial class FavoriteListPage : ContentPage
 
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
             var toast = Toast.Make("Kedvencek adatbázis sikeresen törölve", ToastDuration.Short, 14d);
+            await toast.Show(cancellationTokenSource.Token);
+        }
+    }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        // Check if the platform supports file picking
+        //if (!await FilePicker.IsPickingSupportedAsync())
+        //
+        //    await DisplayAlert("Error", "File picking is not supported on this platform.", "OK")
+        //    return
+        //
+
+        if (Build.VERSION.SdkInt < BuildVersionCodes.R)
+        {
+            var storagePermission = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+            if (storagePermission == PermissionStatus.Unknown || storagePermission == PermissionStatus.Denied)
+            {
+                await DisplayAlert("Error", "Permission Denied", "OK");
+            }
+        }
+
+        var savePath = Path.Combine(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDownloads).AbsolutePath,
+            "AndroKat", "kedvencek.json");
+
+        if (!File.Exists(savePath))
+        {
+            try
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+                File.Create(savePath);
+                File.WriteAllText(savePath, "[]");
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        var jsonFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>> { { DevicePlatform.Android, new[] { "application/json" } } });
+
+        var fileResult = await FilePicker.PickAsync(new PickOptions
+        {
+            FileTypes = jsonFileType,
+            PickerTitle = "Válasszd ki a kedvencek.json fájlt"
+        });
+
+        if (fileResult != null)
+        {
+            var list = await ViewModel.GetFavContentsAsync();
+            string json = System.Text.Json.JsonSerializer.Serialize(list);
+            File.WriteAllText(savePath, json);
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            var toast = Toast.Make("Mentés sikerült", ToastDuration.Short, 14d);
+            await toast.Show(cancellationTokenSource.Token);
+        }
+    }
+
+    private async void Button_Clicked_2(object sender, EventArgs e)
+    {
+        var jsonFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>> { { DevicePlatform.Android, new[] { "application/json" } } });
+
+        var fileResult = await FilePicker.PickAsync(new PickOptions
+        {
+            FileTypes = jsonFileType,
+            PickerTitle = "Válasszd ki a kedvencek.json fájlt"
+        });
+
+        if (fileResult != null)
+        {
+            string fileContent = File.ReadAllText(fileResult.FullPath);
+            var o = System.Text.Json.JsonSerializer.Deserialize<List<FavoriteContentEntity>>(fileContent);
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            var toast = Toast.Make("Olvasás: " + o.Count, ToastDuration.Short, 14d);
             await toast.Show(cancellationTokenSource.Token);
         }
     }
