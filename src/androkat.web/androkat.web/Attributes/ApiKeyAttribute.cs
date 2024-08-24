f2fa021a00217346117e6ec09e8ce1ccf9844cd7
arnold gulyas <arnoldgulyas@gmail.com>
2024-08-24 08:00:32 +0200

    2.82.0: update nugets

--- src/androkat.web/androkat.web/Attributes/ApiKeyAttribute.cs
+++ src/androkat.web/androkat.web/Attributes/ApiKeyAttribute.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace androkat.web.Attributes;
 
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class ApiKeyAttribute : ServiceFilterAttribute
 {
     public ApiKeyAttribute() : base(typeof(ApiKeyAuthorizationFilter))
