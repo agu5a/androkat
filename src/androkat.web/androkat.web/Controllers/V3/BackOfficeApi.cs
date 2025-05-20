856ab1d9749e81a278f24cba5c0b755c2f52472b
Arnold Gulyas <arnoldgulyas@gmail.com>
2025-05-20 06:28:51 +0200

    Add BackOfficeApi controller with error logging functionality

---
+++ src/androkat.web/androkat.web/Controllers/V3/BackOfficeApi.cs
@@ -0,0 +1,33 @@
+﻿using androkat.domain;
+using androkat.domain.Model.WebResponse;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace androkat.web.Controllers.V3;
+
+[EnableRateLimiting("fixed-by-ip")]
+[Route("v3")]
+[ApiController]
+public class BackOfficeApi : ControllerBase
+{
+    private readonly IAdminRepository _apiService;
+
+    public BackOfficeApi(IAdminRepository apiService)
+    {
+        _apiService = apiService;
+    }
+
+    [Route("errors")]
+    [HttpPost]
+    [ProducesResponseType(typeof(bool), 200)]
+    public ActionResult<bool> PostError([FromBody] ErrorRequest error)
+    {
+        if (error is null || string.IsNullOrWhiteSpace(error.Error))
+        {
+            return BadRequest("Hiba");
+        }
+
+        var result = _apiService.InsertError(error);
+        return Ok(result);
+    }
+}
