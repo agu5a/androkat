4887d751279eb9b72d7230ac252c353a390f14dd
Arnold Gulyas <arnoldgulyas@gmail.com>
2025-06-28 04:22:27 +0200

    Enhance ContactViewModel and ContactPage: update version display format, improve Text1 content, and add Instagram link in Text7 for better user engagement.

--- src/androkat.maui/androkat.maui.library/ViewModels/ContactViewModel.cs
+++ src/androkat.maui/androkat.maui.library/ViewModels/ContactViewModel.cs
@@ -1,22 +1,22 @@
-﻿namespace androkat.maui.library.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace androkat.maui.library.ViewModels;
 
 public partial class ContactViewModel : ViewModelBase
 {
     public ContactViewModel()
     {
+        Version = $"verzió: {AppInfo.VersionString}";
+        Text1 = "<b>Célja</b>: Egyesíteni - amennyire lehet - a jelenleg elérhető katolikus mobil alkalmazások nyújtotta lehetőségeket és internetes szolgáltatásokat.<br />A napi imádság/elmélkedés támogatása de nem az idézetek gyűjtögetése.<br />Nem célunk, hogy boltban kapható könyveket ingyen letölthetővé tegyünk.<br />Ne feledjük azt, hogy '... a munkás megérdemli a maga bérét' (Lk 10,7), támogassuk a katolikus könyvkiadókat is.";
+        Text2 = "<b>Fejlesztés, szerkesztés</b>:<br />Gulyás Arnold, Borsó Zsolt";
     }
 
-    public static string Version => AppInfo.VersionString;
+    [ObservableProperty]
+    string version;
 
-    public static string Text1 => "<b>Célja</b>: Egyesíteni – amennyire lehet – a jelenleg elérhető katolikus mobil alkalmazások nyújtotta lehetőségeket és internetes szolgáltatásokat.<br />A napi imádság/elmélkedés támogatása de nem az idézetek gyűjtögetése.<br />Nem célunk, hogy boltban kapható könyveket ingyen letölthetővé tegyünk.<br />Ne feledjük azt, hogy '… a munkás megérdemli a maga bérét' (Lk 10,7), támogassuk a katolikus könyvkiadókat is.";
+    [ObservableProperty]
+    string text1;
 
-    public static string Text2 => "<b>Fejlesztés, szerkesztés</b>:<br />Gulyás Arnold, Borsó Zsolt";
-
-    public static string Text3 => "<a href='https://www.facebook.com/androkat'>facebook</a>";
-
-    public static string Text4 => "<a href='mailto:uzenet@androkat.hu'>uzenet@androkat.hu</a>";
-
-    public static string Text5 => "<a href='https://twitter.com/AndroKat'>twitter</a>";
-
-    public static string Text6 => "<a href='https://www.youtube.com/channel/UCF3mEbdkhZwjQE8reJHm4sg'>youtube</a>";
+    [ObservableProperty]
+    string text2;
 }
--- src/androkat.maui/androkat.maui/Pages/ContactPage.xaml.cs
+++ src/androkat.maui/androkat.maui/Pages/ContactPage.xaml.cs
@@ -9,4 +9,38 @@ public partial class ContactPage : ContentPage
         InitializeComponent();
         BindingContext = viewModel;
     }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "S1075:URIs should not be hardcoded", Justification = "Contact page social links")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "Event handler cannot be static")]
+    private async void OnFacebookClicked(object sender, EventArgs e)
+    {
+        await Browser.Default.OpenAsync("https://www.facebook.com/androkat", BrowserLaunchMode.SystemPreferred);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "Event handler cannot be static")]
+    private async void OnEmailClicked(object sender, EventArgs e)
+    {
+        await Browser.Default.OpenAsync("mailto:uzenet@androkat.hu", BrowserLaunchMode.SystemPreferred);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "S1075:URIs should not be hardcoded", Justification = "Contact page social links")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "Event handler cannot be static")]
+    private async void OnTwitterClicked(object sender, EventArgs e)
+    {
+        await Browser.Default.OpenAsync("https://twitter.com/AndroKat", BrowserLaunchMode.SystemPreferred);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "S1075:URIs should not be hardcoded", Justification = "Contact page social links")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "Event handler cannot be static")]
+    private async void OnYouTubeClicked(object sender, EventArgs e)
+    {
+        await Browser.Default.OpenAsync("https://www.youtube.com/channel/UCF3mEbdkhZwjQE8reJHm4sg", BrowserLaunchMode.SystemPreferred);
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "S1075:URIs should not be hardcoded", Justification = "Contact page social links")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "Event handler cannot be static")]
+    private async void OnInstagramClicked(object sender, EventArgs e)
+    {
+        await Browser.Default.OpenAsync("https://www.instagram.com/androkat_app/", BrowserLaunchMode.SystemPreferred);
+    }
 }
